78916c1bc63313458becab041c362f16ce7c35c0
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2020-11-29 14:18:34 +0200

    Ported over my work from the previous tasks.

---
+++ 20109982_Task_1/GameEngine.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static _20109982_Task_1.Character;
+
+namespace _20109982_Task_1
+{
+    /// <summary>
+    /// Q.3.3
+    /// </summary>
+    class GameEngine
+    {
+
+        public GameEngine(Map map)
+        {
+            Map map = new Map();
+        }
+
+        private Map Map;
+
+        public Map map
+        {
+            get
+            {
+                return Map;
+            }
+            set
+            {
+                map = value;
+            }
+        }
+        public bool MovePlayer(Character.Movement direction)
+        {
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return null;
+        }
+
+        public void EnemyAttacks() 
+        {
+            foreach (var item in Map.myEnemiesArr)
+	{
+
+	}
+        }
+        public void Save()
+        {
+            FileStream outputFile = new FileStream("gameMap.binary", FileMode.Create, FileAccess.Write);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(outputFile, Map);
+            outputFile.Close();
+
+        }
+        public void Load()
+        {
+            FileStream inputFile = new FileStream("gameMap.binary", FileMode.Open, FileAccess.Read);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            Map fromFile= (Map)binaryFormatter.Deserialize(inputFile);
+            inputFile.Close();
+        }
+    }
+}
---
+++ 20109982_Task_1/Gold.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20109982_Task_1
+{
+    /// <summary>
+    /// Q.2.2
+    /// </summary>
+    class Gold : Item
+    {
+        Map myMap = new Map();
+        private int amountOfGold;
+        public int goldDrop
+        {
+            get
+            {
+                return amountOfGold;
+            }
+            set
+            {
+                amountOfGold = value;
+            }
+        }
+
+        Random rng = new Random();
+
+        public Gold (int xPosition, int yPosition) : base()
+        {
+             amountOfGold = rng.Next(6);
+             myMap.mapItemsArr[xPosition,yPosition] = amountOfGold;
+        }
+    }
+}
